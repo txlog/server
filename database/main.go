7752cd71c6d9e5a390c0c279f6ab07c366315643
Rodrigo de Avila <rdeavila@gmail.com>
2025-03-01 21:30:53 +0000

    Implement database migration setup and initial schema

--- database/main.go
+++ database/main.go
@@ -2,13 +2,22 @@ package database
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "github.com/lib/pq"
 )
 
 var Db *sql.DB
+
+//go:embed migrations/*
+var migrationsFS embed.FS
 
 func ConnectDatabase() {
 	psqlSetup := fmt.Sprintf(
@@ -27,6 +36,29 @@ func ConnectDatabase() {
 		panic(errSql)
 	} else {
 		Db = db
-		fmt.Println("Successfully connected to database!")
+		fmt.Println("Successfully connected to database")
 	}
+
+	defer db.Close()
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatalf("Failed to create database driver: %v", err)
+	}
+
+	source, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		log.Fatalf("Failed to create migration source: %v", err)
+	}
+
+	m, err := migrate.NewWithInstance("iofs", source, "postgres", driver)
+	if err != nil {
+		log.Fatalf("Failed to create migration instance: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+
+	fmt.Println("Migrations applied successfully")
 }

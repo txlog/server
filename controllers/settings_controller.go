4e277410aecb72c82310853b466f0fe619030cf2
Rodrigo de Avila <rdeavila@gmail.com>
2025-03-19 23:44:41 +0000

    Update settings controller to include cron retention configurations in the context

--- controllers/settings_controller.go
+++ controllers/settings_controller.go
@@ -10,14 +10,16 @@ import (
 
 func GetSettingsIndex(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "settings.html", gin.H{
-		"Context":                ctx,
-		"title":                  "Server Settings",
-		"pgsqlHost":              os.Getenv("PGSQL_HOST"),
-		"pgsqlPort":              os.Getenv("PGSQL_PORT"),
-		"pgsqlUser":              os.Getenv("PGSQL_USER"),
-		"pgsqlDb":                os.Getenv("PGSQL_DB"),
-		"pgsqlPassword":          util.MaskString(os.Getenv("PGSQL_PASSWORD")),
-		"pgsqlSslmode":           os.Getenv("PGSQL_SSLMODE"),
-		"executionRetentionDays": os.Getenv("EXECUTION_RETENTION_DAYS"),
+		"Context":                  ctx,
+		"title":                    "Server Settings",
+		"pgsqlHost":                os.Getenv("PGSQL_HOST"),
+		"pgsqlPort":                os.Getenv("PGSQL_PORT"),
+		"pgsqlUser":                os.Getenv("PGSQL_USER"),
+		"pgsqlDb":                  os.Getenv("PGSQL_DB"),
+		"pgsqlPassword":            util.MaskString(os.Getenv("PGSQL_PASSWORD")),
+		"pgsqlSslmode":             os.Getenv("PGSQL_SSLMODE"),
+		"cronRetentionDays":        os.Getenv("CRON_RETENTION_DAYS"),
+		"cronRetentionExpression":  os.Getenv("CRON_RETENTION_EXPRESSION"),
+		"cronStatisticsExpression": os.Getenv("CRON_STATS_EXPRESSION"),
 	})
 }
